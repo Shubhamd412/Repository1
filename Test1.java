11057217fce94d007418c299919ce2c457ff8e13
Shubhamd412 <shubhamdesai412@gmail.com>
2022-02-26 19:12:14 +0530

    initial commit

---
+++ Test1.java
@@ -0,0 +1,17 @@
+package shubham.pk;
+
+import java.util.Scanner;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner src=new Scanner(System.in);
+int a=0;
+int b=0;
+a=src.nextInt();
+b=src.nextInt();
+System.out.println(a+b);
+}
+
+}
